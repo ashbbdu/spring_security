148b9467cde508988bde3ca90e447859f618a056
ashbbdu <ashishsrivastava7007@gmail.com>
2026-06-20 22:35:55 +0530

    started ecommerce-adming project

---
+++ src/main/java/com/ecommer_admin/admin_ecommerce/AdminEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommer_admin.admin_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommer_admin/admin_ecommerce/AdminEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommer_admin.admin_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
